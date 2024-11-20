18b3df752db10ac4e5c84521e529fb07b486859e
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-11-20 11:57:17 +0530

    commit

---
+++ Module4/20-11/ServletConfigContextEx/src/main/java/MyServlet2.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet2 extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		ServletContext context = getServletContext();
+		String a = context.getInitParameter("driver");
+		System.out.println(a);
+	}
+}
